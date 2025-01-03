3cd1a3f2797a9ca6697e3eb1b927143ed491f8ea
Niall Buckley <buckleyniall100@gmail.com>
2025-01-03 07:36:44 -0600

    Add meazure lib to the basic server

--- basicserver/testapp/views.py
+++ basicserver/testapp/views.py
@@ -1,11 +1,14 @@
 from django.http import JsonResponse
+import een_web.common.meazure.meazure as meazure
 import requests
+
+meazure.shim_request()
 
 def make_request():
     """A simple function to make an HTTP request."""
     url = "https://jsonplaceholder.typicode.com/posts/1"  # Example API
     response = requests.get(url)
-    return response.json()
+    return dict(response.request.headers)
 
 def test_view(request):
     """View that triggers the `make_request` function."""
